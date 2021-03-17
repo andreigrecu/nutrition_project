aafece7dee6f4d451d4535ed2bba65a1508038c1
Andrei <apandreialex@gmail.com>
2021-03-17 18:51:59 +0200

    added create user route

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,3 +1,4 @@
+import { IsDate, IsEmail, IsString, minLength } from 'class-validator';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { CoreEntity } from './coreEntity';
 
@@ -5,7 +6,22 @@ import { CoreEntity } from './coreEntity';
 export class User extends CoreEntity {
 
     @Column({ length: 25 })
-    fullName: string;
+    @IsString()
+    firstName: string;
 
-    
+    @Column({ length: 35})
+    @IsString()
+    lastName: string;
+
+    @Column({ unique: true, length: 255 })
+    @IsEmail()
+    email: string;
+
+    @Column({ length: 255 })
+    @IsString()
+    password: string;
+
+    @Column({ nullable: true })
+    @IsDate()
+    birthday: Date;
 }
