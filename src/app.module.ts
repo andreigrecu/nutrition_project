ba1204a6c573abf60d005989e438fcd533a23fd8
Andrei <apandreialex@gmail.com>
2021-03-23 13:39:31 +0200

    email service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerModule } from './modules/customer.module';
+import { PugAdapter } from '@nest-modules/mailer';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
@@ -11,6 +13,20 @@ import { CustomerModule } from './modules/customer.module';
     UserModule,
     CustomerModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/project'),
+    MailerModule.forRoot({
+      transport: 'smtps://proiectlicenta2021@gmail.com:andrei123456d@smtp.gmail.com',
+      defaults: {
+        from: '"nest-modules" <proiectlicenta2021@nestjs.com>',
+      },
+      template: {
+        dir: './src/common/email-templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    },
+    )
   ],
 })
 
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,6 +5,7 @@ import { CreateUserDto } from '../dtos/createUser.dto';
 import { ResponseFactory } from '../factories/ResponseFactory';
 import { Response } from 'express';
 import { User } from '../entities/user';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @ApiTags('Users')
 @Controller('users')
@@ -12,6 +13,7 @@ export class UserController {
 
     constructor(
         private userService: UserService,
+        private readonly mailerService: MailerService,
         private readonly responseFactory: ResponseFactory
     ) { }
 
@@ -24,12 +26,24 @@ export class UserController {
         const checkUserAlreadyRegistered = await this.userService.checkUserAlreadyRegistered(createUserDto.email);
         if(checkUserAlreadyRegistered === true)
             return this.responseFactory.forbidden('User already exists', response);
+        else {
+            this
+                .mailerService
+                .sendMail({
+                    to: 'netec84017@gameqo.com',
+                    from: 'proiectlicenta2021@gmail.com',
+                    subject: 'Let`s test email service',
+                    template: 'mainTemplate.html'
+                })
+                .then(() => { console.log("Merge fratilor") })
+                .catch((error) => { console.log(error) })
+        }
 
         const user = await this.userService.create(createUserDto);
 
         if(user)
            return this.responseFactory.ok(user, response);
         
-        return this.responseFactory.notFound({ general_: 'users.user_can`t_be_created' }, response);
+        return this.responseFactory.error({ general_: 'users.user_can`t_be_created' }, response);
     }
 }
