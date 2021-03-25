e1dc5dfe4fa453521a888570467e792b0bc46f84
Andrei <apandreialex@gmail.com>
2021-03-25 14:26:54 +0200

    filter and pagination

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, Body, Get, Put, Delete, Param, Res} from '@nestjs/common';
+import { Controller, Post, Body, Get, Put, Delete, Param, Res, Query} from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { ApiTags } from "@nestjs/swagger";
+import { ApiTags, ApiQuery } from "@nestjs/swagger";
 import { CreateUserDto } from '../dtos/createUser.dto';
 import { ResponseFactory } from '../factories/ResponseFactory';
 import { Response } from 'express';
-import { MailerService } from '@nestjs-modules/mailer';
 import { EmailQueueProducer } from '../producers/emailQueueProducer';
-
+import { PaginationDto } from '../dtos/pagination.dto';
+import { SortDto } from '../dtos/sort.dto';
+import { UserQueryParams } from '../queryparams/userQueryParams';
+import { QueryParamsFilterFactory } from '../factories/queryParamsFilterFactory';
 
 @ApiTags('Users')
 @Controller('users')
@@ -15,8 +17,40 @@ export class UserController {
     constructor(
         private userService: UserService,
         private readonly responseFactory: ResponseFactory,
+        private readonly queryParamsFilterFactory: QueryParamsFilterFactory,
         private emailQueueProducer: EmailQueueProducer,
     ) { }
+
+    @Get()
+    @ApiQuery({ name: 'sort_direction', type: String, required: false, description: " ASC | DESC" })
+    @ApiQuery({ name: 'sort_order', type: String, required: false })
+    @ApiQuery({ name: 'limit', type: Number, required: false })
+    @ApiQuery({ name: 'page', type: Number, required: false })
+    @ApiQuery({ name: 'pagination', type: Boolean, required: false })
+    async getAll(
+        @Query() paginationDto: PaginationDto,
+        @Query() sortDto: SortDto,
+        @Query() userQueryParams: UserQueryParams,
+        @Res() response: Response
+    ): Promise<any> {
+
+        console.log(paginationDto)
+        if(!paginationDto.pagination || paginationDto.pagination === "false") {
+            let users =  await this.userService.getAll(
+                await this.queryParamsFilterFactory.filter(userQueryParams), sortDto.sort_order, sortDto.sort_direction
+            );
+            
+            return this.responseFactory.ok(users, response);
+        }
+        
+        return this.responseFactory.ok(await this.userService.getPaginated(
+            await this.queryParamsFilterFactory.filter(userQueryParams),
+            paginationDto.page,
+            paginationDto.limit,
+            sortDto.sort_order,
+            sortDto.sort_direction
+        ), response);
+    }
 
     @Post('register')
     async create(
---
+++ src/dtos/sort.dto.ts
@@ -0,0 +1,6 @@
+import { Order } from '../common/order';
+
+export class SortDto {
+    sort_order: string;
+    sort_direction: Order;
+}
